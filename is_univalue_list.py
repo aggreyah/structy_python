e285a71202bf7fa40215c137af7f97a9c09726f9
aggreyah <aggreyah@gmail.com>
2022-04-17 09:54:13 +0300

    added implementation of the univalue problem.

---
+++ is_univalue_list.py
@@ -0,0 +1,33 @@
+from node import *
+
+
+def is_univalue_list(head):
+    head_value = head.value
+    current = head
+    while current is not None:
+        if current.value != head_value:
+            return False
+        else:
+            current = current.next
+    return True
+
+
+def is_univalue_list_recursive(head, prev=None):
+    if head is None:
+        return True
+    if prev is not None and head.value != prev.value:
+        return False
+    return is_univalue_list_recursive(head.next, head)
+
+
+if __name__ == "__main__":
+    a = Node(7)
+    b = Node(7)
+    c = Node(7)
+
+    a.next = b
+    b.next = c
+
+    # 7 -> 7 -> 4
+
+    print(is_univalue_list_recursive(a))
